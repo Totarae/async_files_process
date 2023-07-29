3031e5b7fce2babe52fc157487982e4062195497
Totarae <gregoryivanoff6@gmail.com>
2023-07-30 00:15:59 +0300

    Update Example2.py

--- Example2.py
+++ Example2.py
@@ -8,8 +8,12 @@ import random
 import string
 from random import randint
 
+FILES_IN_ARCHIVE = 10
+TOTAL_ZIPS = 5
+CLEAN_UP = False
+
 new_data = None
-event = asyncio.Event()
+events = [asyncio.Event() for _ in range(TOTAL_ZIPS)]  # Create pool of events
 
 
 async def make_file(file_path):
@@ -42,31 +46,36 @@ async def make_file(file_path):
         return False
 
 
-async def task_a():
+async def task_a(event_index):
     tasks = []
-    for i in range(0, 10):
-        filename = f'file_{i}.xml'
+    for i in range(0, FILES_IN_ARCHIVE):
+        filename = f'file_{event_index}_{i}.xml'
         file_path = os.path.join("output", filename)
         task = make_file(file_path)  # Create the task without awaiting
         tasks.append(task)
     await asyncio.gather(*tasks)  # Await all tasks together
-    event.set()
+    events[event_index].set()
 
 
-async def task_b():
-    await event.wait()
+async def task_b(event_index):
+    await events[event_index].wait()
+
     for file in os.listdir("output"):
-        if file.endswith(".xml"):
+        if file.endswith(f"_{event_index}.xml"):
             print(os.path.join("output", file))
-    event.clear()
+
+    print(f'Completed {event_index} event')
+    events[event_index].clear()
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     try:
-        loop.create_task(task_a())
-        loop.create_task(task_b())
-        loop.run_forever()
+        for i in range(TOTAL_ZIPS):
+            loop.create_task(task_a(i))
+            loop.create_task(task_b(i))
+       #loop.run_forever()
+        loop.run_until_complete(asyncio.wait([task_b(i) for i in range(TOTAL_ZIPS)]))
     except KeyboardInterrupt:
         # Stop the event loop gracefully on keyboard interrupt (Ctrl+C)
         for task in asyncio.all_tasks():
